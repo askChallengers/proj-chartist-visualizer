3f334e9ebd2da2cd2f61b8b00cf481f977b68bb7
lks1996 <mosplee@naver.com>
2024-10-12 11:54:52 +0900

    env 설정 추가

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const path = require('path');
+require('dotenv').config();
 
 const app = express();
 const port = 3000;
 
 // 정적 파일 제공 (HTML, CSS, JavaScript)
 app.use(express.static(path.join(__dirname, 'public')));
+
+// 클라이언트에 환경변수 전달
+app.get('/env', (req, res) => {
+  res.json({
+    GOOGLE_SHEET_TOP8_GROUP_URI: process.env.GOOGLE_SHEET_TOP8_GROUP_URI
+  });
+});
 
 // 서버 시작
 app.listen(port, () => {
